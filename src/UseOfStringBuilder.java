67902af88713255185d074295559f78d69aa0b0a
Umair Tariq <umairdev010@gmail.com>
2026-01-12 17:20:18 +0500

    adding new projects on monday evening

--- src/UseOfStringBuilder.java
+++ src/UseOfStringBuilder.java
@@ -1,2 +1,12 @@
 public class UseOfStringBuilder {
+    static void main(String[] args) {
+        System.out.println("WELLCOME TO STRING CONCATENATION WITH THE HELP OF STRING BUILDER");
+        String[] stringArray = {"apple","banana","grapes","watermelon","melon"};
+        StringBuilder namesFruits = new StringBuilder("THE NAMES OF FRUITS IS");
+        for (String fruit:stringArray){
+            namesFruits.append(" ");
+            namesFruits.append(fruit.toUpperCase());
+        }
+        System.out.println(namesFruits);
+    }
 }
