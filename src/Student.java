7d78589ddfd974a07632b9d7ca9e2d96ac777f0d
Umair Tariq <umairdev010@gmail.com>
2026-01-12 17:28:35 +0500

    adding new projects on monday evening

--- src/Student.java
+++ src/Student.java
@@ -1,25 +1,30 @@
 public class Student {
     int age;
     String name;
-    private Student(int age,String name){
+    final int phone;
+    private Student(int age,String name,int phone){
         this.age = age;
         this.name =name;
+        this.phone=phone;
     }
-    static Student makeStudent(int age, String name){
-        return new Student(age,name);
+    static Student makeStudent(int age, String name,int phone){
+        return new Student(age,name,phone);
     }
 
     @Override
     public String toString() {
         return (
                 "THE STUDENT NAME IS " + this.name +
-                        " AND STUDENT AGE IS " + this.age
+                        " AND STUDENT AGE IS " + this.age +
+                        " AND PHONE NUMBER IS " + this.phone
                 );
     }
 
     static void main(String[] args) {
-        Student newStudent = Student.makeStudent(22,"Umair");
-        System.out.println(newStudent);
+        Student newStudent1 = Student.makeStudent(22,"Umair",342);
+        Student newStudent2 = Student.makeStudent(22,"Umair",234);
+        System.out.println(newStudent1);
+        System.out.println(newStudent2);
     }
 
 }
