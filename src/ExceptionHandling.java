59441ebcbe8118059e5b9fa5bcab896a3254ed20
Umair Tariq <umairdev010@gmail.com>
2026-01-16 15:52:42 +0500

    adding new project on friday

---
+++ src/ExceptionHandling.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    static void main(String[] args) {
+        try {
+            System.out.println("WELLCOME TO EXCEPTIONAL HANDLING IN JAVA");
+            Scanner input = new Scanner(System.in);
+            System.out.print("PLEASE ENTER THE FIRST NUMBER : ");
+            int var1st = input.nextInt();
+            System.out.print("PLEASE ENTER THE SECOND NUMBER : ");
+            int var2nd = input.nextInt();
+            System.out.printf("THE DIVISION OF TWO NUMBER IS %d", var1st/var2nd);
+        } catch (ArithmeticException e){
+            System.out.printf("THERE IS A ERROR : %s",e.getMessage());
+        } catch (Exception e){
+            System.out.printf("THERE IS A Exception ERROR : %s",e.getMessage());
+        }
+    }
+}
