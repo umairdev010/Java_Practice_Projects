10ada6ba0ad537a3c35522378520410aca2936f4
Umair Tariq <umairdev010@gmail.com>
2026-01-18 05:41:07 +0500

    changing in one project of Queue

--- src/QueueProject.java
+++ src/QueueProject.java
@@ -1,2 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueProject {
+    static void main(String[] args) {
+        System.out.println("WELLCOME TO LEARNING QUEUE.");
+        Queue<String> listOfTasks = new LinkedList<>();
+        listOfTasks.offer("counting books");
+        listOfTasks.offer("counting apples");
+        listOfTasks.offer("counting glass");
+        listOfTasks.offer("counting flowers");
+        System.out.println(listOfTasks.peek());
+        System.out.println(listOfTasks.poll());
+        System.out.println(listOfTasks.poll());
+        System.out.println(listOfTasks.poll());
+        System.out.println(listOfTasks.poll());
+        System.out.println(listOfTasks.poll());
+        System.out.println(listOfTasks.poll() == null);
+        System.out.println(listOfTasks);
+    }
 }
